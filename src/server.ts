68b169e142beb7e42038aa015dd10688023217c0
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-05-22 21:42:20 +0600

    connect mongodb and create server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app"
+import confiqe from "./app/confiqe"
+
+
+const main = async()=>{
+    try {
+        await mongoose.connect(confiqe.database_URL as string);
+        app.listen( confiqe.port , () => {
+            console.log(`Example app listening on port ${confiqe.port}`)
+          })
+    } catch (error) {
+       console.log(error) 
+    }
+}
+
+main()
