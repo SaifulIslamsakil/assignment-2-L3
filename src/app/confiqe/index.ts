9bb2de9b4ea31a7ffda60ca151557a452fe81f57
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-05-22 21:23:04 +0600

    delete all file

--- src/app/confiqe/index.ts
+++
@@ -1,9 +0,0 @@
-import env from "dotenv"
-import path from "path"
-
-env.config(({ path: path.join(process.cwd(), ".env") }))
-
-export default {
-    port: process.env.PORT,
-    database_URL : process.env.DATABASE_URL
-}
